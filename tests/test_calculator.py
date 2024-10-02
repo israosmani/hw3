b984fe060170fde564764dafa7c60ad371cdfc01
root <root@Isra>
2024-10-02 10:28:27 -0400

    add files for part2

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,3 +3,7 @@ def test_addition():
     assert add(2,2) == 4
 def test_subtraction():
     assert subtract(2,2) == 0
+def test_multiply():
+    assert Calculator.multiply(2,2) == 4
+def test_divide():
+    assert Calculator.divide(2,2) == 1
